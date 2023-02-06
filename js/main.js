5c2ea9c1b47d56569f214668383a69b72d2a9853
Gyrefallon <gonzalo.2800@gmail.com>
2023-02-06 09:24:51 -0300

    Mejoras 06/02

--- js/main.js
+++ js/main.js
@@ -26,6 +26,21 @@ function numero(numeros){
     }
     if ((numeros.target.value>9) && (numeros.target.value<=10)){
         valueInput.innerText = "Excelente";
-    }
-}
+    };}
+    
+
+$( function() {
+        $( "#slider-range" ).slider({
+          range: true,
+          min: 1000,
+          max: 100000,
+          step: 1000,
+          values: [ 1000, 3000 ],
+          slide: function( event, ui ) {
+            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+          }
+        });
+        $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+          " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+      } );
 
